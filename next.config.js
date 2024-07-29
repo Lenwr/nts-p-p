4664a9e13f6674d7417e5280078c3ada4a6a0a32
Lenwr <saidabou.lateam@gmail.com>
2024-07-29 08:32:28 +0200

    nextConfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        MY_API_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+        DATABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    },
+};
 
 module.exports = nextConfig;
